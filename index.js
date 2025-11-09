d81e0ba08807c80c04ba60401b6e9a0c7156decd
Razib Das <it.razib.mbstu@gmail.com>
2025-11-09 14:34:50 +0600

    Added Crud POST Method

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // ahPprnxWq5QgvtNY
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d4mqfiw.mongodb.net/?appName=Cluster0`;
 
@@ -19,27 +18,38 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 async function run() {
   try {
-    
     await client.connect();
-   
+    const db = client.db('heroHomeDB');
+    const Services = db.collection("Services");
+
+    app.post("/services", async (req, res) => {
+      const newService = req.body;
+
+      try {
+        const result = await Services.insertOne(newService);
+        res.status(200).send(result);
+      } catch (error) {
+        res.status(500).send({ message: "Do not Added the services" });
+      }
+    });
+
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-    // Ensures that the client will close when you finish/error
-  
   }
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send("Home Hero Server Running")
-})
+app.get("/", (req, res) => {
+  res.send("Home Hero Server Running");
+});
 
 app.listen(port, () => {
-    console.log(`Home Hero Server Runnin On port ${port}`)
-})
+  console.log(`Home Hero Server Runnin On port ${port}`);
+});
