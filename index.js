1c93bc966eaaffd9c4534272f298e6a5bb41e2e2
Razib Das <it.razib.mbstu@gmail.com>
2025-11-09 13:45:30 +0600

    Added MongoDB

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 
 const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+
+// ahPprnxWq5QgvtNY
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d4mqfiw.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    
+    await client.connect();
+   
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+  
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send("Home Hero Server Running")
