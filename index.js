5a3d6d3ce6062ddf53e6cdfa320071c9f687be11
Razib Das <it.razib.mbstu@gmail.com>
2025-11-11 22:54:20 +0600

    Added filter using  and

--- index.js
+++ index.js
@@ -23,70 +23,86 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const db = client.db('heroHomeDB');
+    const db = client.db("heroHomeDB");
     const Services = db.collection("Services");
-    const MyBooking = db.collection("MyBooking")
+    const MyBooking = db.collection("MyBooking");
 
-
-     app.get('/services', async(req, res) => {
+    app.get("/services", async (req, res) => {
       const email = req.query.email;
-      const query = {}
-      if(email){
+      const query = {};
+      if (email) {
         query.providerEmail = email;
       }
-      const cursor = Services.find(query)
-      const result  = await cursor.toArray()
-      res.send(result)
-    })
+      const cursor = Services.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-   
-
-    app.get('/services/:id', async(req, res) => {
+    app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await Services.findOne(query)
-      res.send(result)
-    })
+      const query = { _id: new ObjectId(id) };
+      const result = await Services.findOne(query);
+      res.send(result);
+    });
 
+    app.get("/services", async (req, res) => {
+      const cursor = Services.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    app.get('/services', async(req, res) => {
-        const cursor = Services.find();
-        const result = await cursor.toArray()
-        res.send(result)
-    })
+    app.get("/filter", async (req, res) => {
+      const minPrice = parseInt(req.query.minPrice);
+      const maxPrice = parseInt(req.query.maxPrice);
 
-    app.get('/my-booking', async(req, res) => {
-      const email = req.query.email
-      const query = {}
-      if(email){
-        query.email = email
+      let query = {};
+      if (!isNaN(maxPrice) && !isNaN(minPrice)) {
+        query = { price: { $gte: minPrice, $lte: maxPrice } };
       }
 
-      const cursor = MyBooking.find(query)
-      const result = await cursor.toArray()
-      res.send(result)
-    })
+      const allServices = await Services.find().toArray();
 
-     app.get("/my-booking/:id", async(req, res) => {
+      let filtered = allServices;
+      if (query.price) {
+        filtered = allServices.filter((item) => {
+          const price = parseInt(item.price); 
+          return price >= minPrice && price <= maxPrice;
+        });
+      }
+
+      res.send(filtered);
+    });
+
+    app.get("/my-booking", async (req, res) => {
+      const email = req.query.email;
+      const query = {};
+      if (email) {
+        query.email = email;
+      }
+
+      const cursor = MyBooking.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/my-booking/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await MyBooking.findOne(query)
-      res.send(result)
-    })
+      const query = { _id: new ObjectId(id) };
+      const result = await MyBooking.findOne(query);
+      res.send(result);
+    });
 
-     app.get('/my-booking', async(req, res) => {
+    app.get("/my-booking", async (req, res) => {
       const cursor = MyBooking.find();
       const result = await cursor.toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-     
-
-       app.get('/service', async(req, res) => {
-        const cursor = Services.find().limit(6)
-        const result = await cursor.toArray()
-        res.send(result)
-    })
+    app.get("/service", async (req, res) => {
+      const cursor = Services.find().limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
     app.post("/services", async (req, res) => {
       const newService = req.body;
@@ -99,28 +115,26 @@ async function run() {
       }
     });
 
-    app.post('/my-booking', async(req, res) => {
+    app.post("/my-booking", async (req, res) => {
       const newBooking = req.body;
-      const result = await MyBooking.insertOne(newBooking)
-      res.send(result)
-    })
+      const result = await MyBooking.insertOne(newBooking);
+      res.send(result);
+    });
 
-     app.delete('/my-booking/:id', async(req, res) => {
-        const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await MyBooking.deleteOne(query)
-      res.send(result)
-    })
+    app.delete("/my-booking/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await MyBooking.deleteOne(query);
+      res.send(result);
+    });
 
-    app.delete('/services/:id', async(req, res) => {
-      const id = req.params.id
-      const query = {_id: new ObjectId(id)}
-      const result = await Services.deleteOne(query)
-      res.send(result)
-    })
+    app.delete("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await Services.deleteOne(query);
+      res.send(result);
+    });
 
-
-    
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
